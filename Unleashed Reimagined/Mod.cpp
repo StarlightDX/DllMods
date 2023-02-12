c994139c83cb468a4981f2ec7548cac9a5358413
Jessie <101757745+Sticks-Unleashed@users.noreply.github.com>
2023-02-12 02:57:21 +0000

    Update Mod.cpp

--- Unleashed Reimagined/Mod.cpp
+++ Unleashed Reimagined/Mod.cpp
@@ -2,6 +2,7 @@ std::string hudtype;
 std::string upadd1type;
 std::string upadd2type;
 std::string upadd3type;
+std::string setexadabattype; 
 std::string stagepacktype;
 
 extern "C" __declspec(dllexport) void Init()
@@ -11,6 +12,7 @@ extern "C" __declspec(dllexport) void Init()
      upadd1type = reader.Get("Main", "IncludeDir0", "UPDeluxe");
      upadd2type = reader.Get("Main", "IncludeDir1", "Off");
      upadd3type = reader.Get("Main", "IncludeDir2", "Off");
+	 setexadabattype = reader.Get("Main", "IncludeDir3", "Off");
      stagepacktype = reader.Get("Main", "IncludeDir4", "Unleashed Project");
 	ArchiveTreePatcher::Install();
 }
@@ -21,8 +23,8 @@ extern "C" __declspec(dllexport) void PostInit()
 		MessageBoxA(nullptr, "Sonic Unleashed HUD is required to be loaded under Reimagined to use Encore HUD.", "Encore HUD", MB_ICONERROR);	
 		exit(-1);
 	}
-	if (!StringHelper::Compare(stagepacktype, "Unleashed Project") && (!StringHelper::Compare(upadd1type, "Off") || !StringHelper::Compare(upadd2type, "Off") || !StringHelper::Compare(upadd3type, "Off"))) {
-		MessageBoxA(nullptr, "Unleashed Project mode is required for this option to be enabled, please disable this option when using different stage packs.", "Sonic Unleashed: Reimagined", MB_ICONERROR);
+	if (!StringHelper::Compare(stagepacktype, "Unleashed Project") && (!StringHelper::Compare(upadd1type, "Off") || !StringHelper::Compare(upadd2type, "Off") || !StringHelper::Compare(upadd3type, "Off") || !StringHelper::Compare(setexadabattype, "Off"))) {
+		MessageBoxA(nullptr, "Unleashed Project mode is required for additional content, please disable this option when using different stage packs.", "Sonic Unleashed: Reimagined", MB_ICONERROR);
 		exit(-1);
 	}
 }
