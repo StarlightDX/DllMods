6308c1e72700789bc24a8480e0cc6d63f9f759a6
Jessie <101757745+Sticks-Unleashed@users.noreply.github.com>
2023-02-12 11:13:08 +0000

    6

--- Generations Reimagined/Mod.cpp
+++ Generations Reimagined/Mod.cpp
@@ -1,21 +1,11 @@
 std::string hudtype;
 std::string sonictype;
-std::string upadd1type;
-std::string upadd2type;
-std::string upadd3type;
-std::string setexadabattype; 
-std::string stagepacktype;
 
 extern "C" __declspec(dllexport) void Init()
 {
 	INIReader reader("mod.ini");
      hudtype = reader.Get("Main", "IncludeDir6", "Off");
 	 sonictype = reader.Get("Main", "IncludeDir5", "Off");
-     upadd1type = reader.Get("Main", "IncludeDir0", "UPDeluxe");
-     upadd2type = reader.Get("Main", "IncludeDir1", "Off");
-     upadd3type = reader.Get("Main", "IncludeDir2", "Off");
-	 setexadabattype = reader.Get("Main", "IncludeDir3", "Off");
-     stagepacktype = reader.Get("Main", "IncludeDir4", "Unleashed Project");
 	ArchiveTreePatcher::Install();
 }
 extern "C" __declspec(dllexport) void PostInit()
@@ -27,10 +17,6 @@ extern "C" __declspec(dllexport) void PostInit()
 	}
 	if (!StringHelper::Compare(sonictype, "Off") && GetModuleHandle(TEXT("GenerationsD3D11.dll")) == nullptr) {
 		MessageBoxA(nullptr, "Direct3D 11 is required to use Encore Sonic, please enable Direct3D 11 and retry, or disable Encore Sonic.", "Encore Sonic", MB_ICONERROR);
-		exit(-1);
-	}
-	if (!StringHelper::Compare(stagepacktype, "Unleashed Project") && (!StringHelper::Compare(upadd1type, "Off") || !StringHelper::Compare(upadd2type, "Off") || !StringHelper::Compare(upadd3type, "Off") || !StringHelper::Compare(setexadabattype, "Off"))) {
-		MessageBoxA(nullptr, "Unleashed Project mode is required for additional content, please disable this option when using different stage packs.", "Sonic Unleashed: Reimagined", MB_ICONERROR);
 		exit(-1);
 	}
 }
